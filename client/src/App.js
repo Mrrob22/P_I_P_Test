a41a0319b91742d7b36221dc1e0980703b14efad
Mrrob22 <misha1010203@gmail.com>
2024-05-08 18:19:31 +0300

    first commit -> initialized project & created base server

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'materialize-css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {useRoutes} from "./routes";
+
+function App() {
+    const routes = useRoutes(false)
+  return (
+      <Router>
+        <div className="container">
+            {routes}
+        </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {LinksPage} from "./pages/LinksPage";
+import {DetailPage} from "./pages/DetailPage";
+import {CreatePage} from "./pages/CreatePage";
+import {AuthPage} from "./pages/AuthPage";
+
+export const useRoutes = isAuthenticated => {
+    if (isAuthenticated){
+        return (
+            <Routes>
+                <Route path="/links" exact element={<LinksPage/>}></Route>
+                <Route path="/create" exact element={<CreatePage/>}></Route>
+                <Route path="/detail/:id" element={<DetailPage/>}></Route>
+
+                {/*<Route path="/" element={user ? <AuthPage /> : <Navigate to="/login" />} />*/}
+            </Routes>
+        )
+    }
+    return (
+        <Routes>
+            <Route path="/" exact element={<AuthPage/>}></Route>
+
+            {/*<Route path="/" element={user ? <AuthPage /> : <Navigate to="/login" />} />*/}
+        </Routes>
+    )
+}
